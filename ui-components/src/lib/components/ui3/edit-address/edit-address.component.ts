f2ef2de6e9ec4840a8afc1a95c548bb3dec88106
Hassan <hassan.elmallouky@gmail.com>
2024-01-08 16:43:46 +0000

    fix more issues in contact person dialog

--- ui-components/src/lib/components/ui3/edit-address/edit-address.component.ts
+++ ui-components/src/lib/components/ui3/edit-address/edit-address.component.ts
@@ -112,6 +112,8 @@ export class EditAddressComponent implements OnInit{
 
   ngOnInit(): void {
     console.log("inputType: ",this.data.inputType);
+    console.log("content: ",this.data.content);
+    //this.items.controls=this.data.content;
   }
 }
 
